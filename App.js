74420569deb8b58152b89b65019b89a076e857f0
Matias <lodigiani.matias97@gmail.com>
2022-09-27 14:34:31 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import Header from './components/header';
+import { colors } from './constants/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gray',
+  },
+  text: {
+    color: 'black',
+    fontSize: 25,
+  },
+});
+
+export default function App() {
+
+  const [loaded] = useFonts({
+    'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
+    'Lato-Bold': require('./assets/fonts/Lato-Bold.ttf'),
+    'Lato-Light': require('./assets/fonts/Lato-Light.ttf'),
+    'Lato-Italic': require('./assets/fonts/Lato-Italic.ttf'),
+    'Lato-Black': require('./assets/fonts/Lato-Black.ttf'),
+  });
+
+
+  return (
+    <View style={styles.container}>
+      <Header title={'Bienvenido'}/>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
